1398b7cd6bd1bd8bdcd533c246a05931890181cc
DESKTOP-3KGAFP0\Choisw <sw59916@gmail.com>
2022-07-28 13:40:02 +0900

    초기 MITK Module 프로젝트 Commit

---
+++ Modules/ExampleModule/files.cmake
@@ -0,0 +1,25 @@
+set(CPP_FILES
+  ExampleModule.cpp
+  MainViewer.cpp
+  CustomSliceWidget.cpp
+  PlaneInteractor.cpp
+)
+
+set(HEADER_FILES
+  ExampleModule.h
+  MainViewer.h
+  CustomSliceWidget.h
+  PlaneInteractor.h
+)
+
+set(UI_FILES
+)
+
+set(MOC_H_FILES
+  include/CustomSliceWidget.h
+)
+
+set(RESOURCE_FILES
+  Interactions/PlaneMove.xml
+  Interactions/PlaneMoveConfig.xml
+)
